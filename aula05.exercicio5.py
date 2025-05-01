ca823becd20623e091ac5e6f39687ba3e4756566
Rafael Valente <r31273723@gmail.com>
2025-05-01 20:50:07 -0300

    aula05.exercicio5

---
+++ aula05.exercicio5.py
@@ -0,0 +1,9 @@
+num = int(input("Digite um primeiro número:"))
+num2 = int(input("Digite um segundo número:"))
+num3 = int(input("Digite um terceiro número:"))
+if num > num2 and num > num3:
+    print(num)
+elif num2 > num and num2 > num3:
+    print(num2)
+else:
+    print(num3)
