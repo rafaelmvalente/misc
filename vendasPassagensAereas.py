6e1e57c45a5c77931c39948404d36084484ce27b
Rafael Valente <r31273723@gmail.com>
2025-05-12 18:25:32 -0300

    vendasPassagensAereas.py

--- vendasPassagensAereas.py
+++ vendasPassagensAereas.py
@@ -3,28 +3,56 @@ passageiros = {}
 voosDisponiveis = []
 passageirosVoo = {}
 
-print("SISTEMAS DE PASSAGENS AÉREAS:")
-print("\n 1. Cadastrar voo")
-print("\n 2. Consultar voo")
-print("\n 3. Listar passageiros de um voo")
-print("\n 4. Vender passagem")
-print("\n 5. Cancelar passagem")
-print("\n 6. Sair")
+sistema = ""
+while sistema != "0":
+    print("SISTEMAS DE PASSAGENS AÉREAS:")
+    print("\n 1. Cadastrar voo")
+    print("\n 2. Consultar voo")
+    print("\n 3. Listar passageiros de um voo")
+    print("\n 4. Vender passagem")
+    print("\n 5. Cancelar passagem")
+    print("\n 6. Sair")
 
-opcao = input("Escolha uma dessas opções: ")
+    opcao = input("Escolha uma dessas opções: ")
 
-if opcao == "1":
-    numero_voo = int(input("Digite o número do voo: "))
-    vooExiste = 0
-for chave in voos:
-    if chave == numero_voo:
-        vooExiste = 1
-    if vooExiste == 1:
-        print("Voo já cadastrado!")
-    else:
+    if opcao == "1":
+        numeroVoo = int(input("Digite o número do voo: "))
         origem = input("Cidade origem: ")
         destino  = input("Cidade destino: ")
         escalas = int(input("Número de escalas: "))
-        preco = float(input("Preço da passagem: "))
+        preco = float(input("Preço da passagem, em reais: "))
         lugares = int(input("Quantidade de lugares disponíveis: "))
-        
+        voos[numeroVoo] = {"origem": origem, "destino": destino, "escalas": escalas, "preco": preco, "lugares": lugares}
+        voosDisponiveis.append(numeroVoo)
+        passageirosVoo[numeroVoo] = []
+        print("Voo cadastrado com sucesso")
+        vooExiste = 0
+    for chave in voos:
+        if chave == numeroVoo:
+            vooExiste = 1
+        if vooExiste == 1:
+            print("Voo já cadastrado!")
+
+    if opcao == "2":
+        print("\n TIPOS DE CONSULTA:")
+        print("1. Código de Voo")
+        print("2. Cidade origem")
+        print("3. Cidade destino")
+        print("4. Voo com menor escala")
+
+        tipoConsulta = input("Escolha uma dessas opções: ")
+        if tipoConsulta == "1":
+            numeroVoo = int(input("Digite o número do voo: "))
+            print(f"Cidade origem: {origem}")
+            print(f"Cidade destino: {destino}")
+            print(f"Número de escalas: {escalas}")
+            print(f"Preço da passagem: {preco}")
+            print(f"Lugares: {lugares}")
+        else:
+            print("Voo não encontrado :( ")
+
+        if tipoConsulta == "2":
+            cidadeOrigem = input("Qual a cidade origem? ")
+            print(f"Número do voo = {numeroVoo}")
+            print(f"Cidade destino: {destino}")
+            print(f"Preço: {preco}")
